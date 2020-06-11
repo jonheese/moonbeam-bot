92a0e4c356b3bafe1bdaa2fce956325583e311cf
Jon Heese <git@jonheese.com>
2020-06-11 16:25:03 -0400

    Make moonbeam store messages in db

--- moonbeam-bot.py
+++ moonbeam-bot.py
@@ -1,10 +1,16 @@
+import json
+import mysql.connector
+import re
+import requests
+import traceback
+from mysql.connector import Error
 from time import sleep, time
 from slackclient import SlackClient
 from random import randint, seed
 from config import BOT_ID, BOT_TOKEN, JHEESE_ID, JHEESE_CHANNEL_ID, MASHAPE_KEY
-from lxml import html
+from config import DB_HOST, DB_NAME, DB_USER, DB_PASSWORD
 from profanity import profanity
-import requests, json, re, traceback, string
+
 
 #             Name              Image URL                                                   Frequency score     Words
 moonbeams = [["Moonbeam",       "https://slack-files.com/T0TGU21T2-FFYTWBGA2-4e153c7af7",   3,                  None],
@@ -31,6 +37,7 @@ with open('/usr/local/moonbeam-bot/pleasantries.json', 'r') as f:
 
 with open('/usr/local/moonbeam-bot/quotes.json', 'r') as f:
     quotes = json.load(f)
+
 
 def get_ten_new_words():
     return get_ten_new_words(3)
@@ -151,7 +158,7 @@ def get_covid_stats():
             line_count += 1
 
 
-def post_covid(channel, stat):
+def post_covid(channel, stat, slack_client):
     get_covid_stats()
     global covid_stats
     if stat == 'rates':
@@ -169,11 +176,12 @@ def post_covid(channel, stat):
             "The COVID-19 recovery rate is *%s%%* worldwide, and *%s%%* in the US.\n" + \
             "The COVID-19 case rate is *%s%%* worldwide, and *%s%%* in the US.\n" + \
             "The COVID-19 chance of death is *%s%%* worldwide, and *%s%%* in the US.") % \
-            (world_death_rate, us_death_rate, world_recovery_rate, us_recovery_rate, world_case_rate, us_case_rate, world_death_chance, us_death_chance))
+            (world_death_rate, us_death_rate, world_recovery_rate, us_recovery_rate, world_case_rate, us_case_rate, world_death_chance, us_death_chance),
+            slack_client=slack_client)
     else:
         percentage = round(float(covid_stats['us'][stat].replace(',','')) * 100 / float(covid_stats['world'][stat].replace(',','')), 2)
         post_message(channel, "There are currently *%s* COVID-19 %s worldwide, with *%s (%s%%)* of those %s in the US" % \
-            (covid_stats['world'][stat], stat, covid_stats['us'][stat], percentage, stat))
+            (covid_stats['world'][stat], stat, covid_stats['us'][stat], percentage, stat), slack_client=slack_client)
 
 
 def check_for_match(word, dictionary):
@@ -184,13 +192,64 @@ def check_for_match(word, dictionary):
     return False
 
 
-def post_message(channel, text=None, attachments=None, as_user=True):
+def post_message(channel, text=None, attachments=None, as_user=True, slack_client=None):
     print("posting message in channel %s (as_user=%s):" % (channel, as_user))
     print(text.encode('utf-8').strip())
     slack_client.api_call("chat.postMessage", channel=channel, text=text, attachments=attachments, as_user=as_user)
 
 
-if __name__ == "__main__":
+def store_message(data, conn, cache=None):
+    if not cache:
+        cache = {
+            "users": {},
+            "channels": {},
+            "teams": {},
+        }
+    timestamp = data["ts"].split(".")[0]
+    if "bot_id" in data.keys():
+        return cache
+    try:
+        slack_user_id = data["user"]
+    except KeyError:
+        print(json.dumps(data, indent=2))
+        return cache
+    try:
+        slack_team_id = data["team"]
+    except KeyError as e:
+        return cache
+    slack_channel_id = data["channel"]
+    text = data["text"]
+    files = []
+    if "files" in data.keys():
+        files = data['files']
+    if "file" in data.keys():
+        files.append(data['file'])
+
+    if not slack_team_id in cache["teams"].keys():
+        team_id = insert("tbl_teams", "slack_team_id", slack_team_id, "slack_team_id", slack_team_id, conn)
+        cache["teams"][slack_team_id] = team_id
+    else:
+        team_id = cache["teams"][slack_team_id]
+    if not slack_user_id in cache["users"].keys():
+        user_id = insert("tbl_users", ["slack_user_id", "team_id"], [slack_user_id, team_id], "slack_user_id", slack_user_id, conn)
+        cache["users"][slack_user_id] = user_id
+    else:
+        user_id = cache["users"][slack_user_id]
+    if not slack_channel_id in cache["channels"].keys():
+        channel_id = insert("tbl_channels", ["slack_channel_id", "team_id"], [slack_channel_id, team_id], "slack_channel_id", slack_channel_id, conn)
+        cache["channels"][slack_channel_id] = channel_id
+    else:
+        channel_id = cache["channels"][slack_channel_id]
+    message_id = insert_message(team_id, channel_id, user_id, timestamp, text, conn)
+    handle_files(files, message_id, conn)
+    if "user_profile" in data.keys():
+        handle_user_profile(data['user_profile'], user_id, conn)
+    return cache
+
+
+def main():
+    conn = init_db()
+    cache = None
     slack_client = SlackClient(BOT_TOKEN)
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
 #    reset_moonbeam()
@@ -202,37 +261,39 @@ if __name__ == "__main__":
                     output_list = slack_client.rtm_read()
                     if output_list and len(output_list) > 0:
                         for output in output_list:
-                            if output and 'text' in output and ('bot_id' not in output or output['bot_id'] != BOT_ID) \
-                                    and ('username' not in output or output['username'] != "slackbot") \
-                                    and ('is_ephemeral' not in output or output['is_ephemeral'] != "true"):
+                            if output and 'text' in output and ('is_ephemeral' not in output or output['is_ephemeral'] != "true"):
+                                cache = store_message(output, conn, cache)
+                                if ('bot_id' in output and output['bot_id'] == BOT_ID) or ('username' in output and output['username'] == "slackbot"):
+                                    continue
                                 print(json.dumps(output, indent=2))
                                 # Control message from jheese
                                 if 'channel' in output and output['channel'] == JHEESE_CHANNEL_ID and 'user' in output and output['user'] == JHEESE_ID:
                                     command = output['text']
                                     if command == "words":
-                                        post_message(channel=JHEESE_CHANNEL_ID, text=get_moonbeam_words(), as_user=False)
+                                        post_message(channel=JHEESE_CHANNEL_ID, text=get_moonbeam_words(), as_user=False, slack_client=slack_client)
                                     elif command.startswith("reset"):
                                         if len(command.split(" ")) == 1:
                                             moonbeam_name = None
                                         else:
                                             moonbeam_name = " ".join(command.split(" ")[1:])
                                         reset_moonbeam(moonbeam_name)
-                                        post_message(channel=JHEESE_CHANNEL_ID, text=get_moonbeam_words(moonbeam_name), as_user=False)
+                                        post_message(channel=JHEESE_CHANNEL_ID, text=get_moonbeam_words(moonbeam_name), as_user=False, slack_client=slack_client)
                                     if command.split()[0] == "post":
                                         room_name = command.split()[1]
                                         message = " ".join(command.split()[2:])
-                                        post_message(channel=room_name, text=message)
+                                        post_message(channel=room_name, text=message, slack_client=slack_client)
                                 # Quotable request
                                 if "quotable" in output['text'].lower():
                                     quote = get_quote_message()
                                     print("printing quotable:")
                                     print(quote)
-                                    post_message(channel=output['channel'], text=quote)
+                                    post_message(channel=output['channel'], text=quote, slack_client=slack_client)
                                 # General command request
                                 action = ""
                                 if output['text'].lower() == "yes, have some":
                                     post_message(channel=output['channel'], \
-                                        text="http://yeshavesome.tv.inetu.org")
+                                        text="http://yeshavesome.tv.inetu.org",
+                                        slack_client=slack_client)
                                 if "covid-stats" in output['text'].lower():
                                     post_covid(channel=output['channel'], stat='deaths')
                                     post_covid(channel=output['channel'], stat='recovered')
@@ -281,27 +342,27 @@ if __name__ == "__main__":
                                             if len(quote.split(" - ")) != 2:
                                                 raise Exception("No dash in quote")
                                             add_quote(quote)
-                                            post_message(channel=output['channel'], text="Okay <@%s>, I added the following Quotable to my database:" % output['user'])
-                                            post_message(channel=output['channel'], text=get_quote_message(quote))
+                                            post_message(channel=output['channel'], text="Okay <@%s>, I added the following Quotable to my database:" % output['user'], slack_client=slack_client)
+                                            post_message(channel=output['channel'], text=get_quote_message(quote), slack_client=slack_client)
                                         except Exception as e:
                                             random_quote = get_quote()
                                             post_message(channel=output['channel'], \
                                                 text="<@%s>, to add a quote to my database, your message must follow the format: `Moonbeam add-quote <quote> - <attribution>`, " % \
-                                                output['user'] + ", eg. `Moonbeam add-quote %s`" % random_quote)
+                                                output['user'] + ", eg. `Moonbeam add-quote %s`" % random_quote, slack_client=slack_client)
                                             raise e
                                         continue
                                     if action == "help":
                                         post_message(channel=output['channel'], \
                                             text="I understand the following commands: `Moonbeam roll <dice>`, `Moonbeam add-quote <quote> - <attribution>`, " + \
-                                            "`covid-stats`, `covid-deaths`, `covid-cases`, `covid-recovery`, `covid-rates`, `quotable`")
+                                            "`covid-stats`, `covid-deaths`, `covid-cases`, `covid-recovery`, `covid-rates`, `quotable`", slack_client=slack_client)
                                         continue
                                     if rude:
                                         post_message(channel=output['channel'], \
-                                                text="There's no need to be rude, <@%s>! :face_with_raised_eyebrow:" % output['user'])
+                                                text="There's no need to be rude, <@%s>! :face_with_raised_eyebrow:" % output['user'], slack_client=slack_client)
                                         break
                                     if not pleased:
                                         post_message(channel=output['channel'], \
-                                                text="Ah ah ah, <@%s>, you didn't say the magic word... :face_with_monocle:" % output['user'])
+                                                text="Ah ah ah, <@%s>, you didn't say the magic word... :face_with_monocle:" % output['user'], slack_client=slack_client)
                                         break
                                     if action == "roll":
                                         summary = "*Dice*    \t\t\t\t*Rolls*"
@@ -358,11 +419,11 @@ if __name__ == "__main__":
                                         attachments = {"text":"<@%s> rolled a *%s*\n%s" % (output['user'], total_string, summary), "color":color}
                                         if image_url:
                                             attachments['image_url'] = "https://slack-files.com/T0TGU21T2-FMLC3CUFL-04242147ee"
-                                        post_message(channel=output['channel'], text='', attachments=[attachments])
+                                        post_message(channel=output['channel'], text='', attachments=[attachments], slack_client=slack_client)
                                     else:
                                         print("action was %s" % action)
                                         post_message(channel=output['channel'], \
-                                                text="<@%s>, I'm not really sure what \"%s\" means..." % (output['user'], output['text']))
+                                                text="<@%s>, I'm not really sure what \"%s\" means..." % (output['user'], output['text']) ,slack_client=slack_client)
                                     break
                                 else:
                                     if "covid-deaths" in output['text'].lower():
@@ -383,7 +444,7 @@ if __name__ == "__main__":
                                                 if word == message_word:
                                                     print(json.dumps(output, indent=2))
                                                     post_message(channel=output['channel'], \
-                                                            text="%s because: *%s*\n%s" % (moonbeam[0], word, moonbeam[1]))
+                                                            text="%s because: *%s*\n%s" % (moonbeam[0], word, moonbeam[1]), slack_client=slack_client)
                                                     reset_moonbeam(moonbeam[0])
                                                     break
                     sleep(READ_WEBSOCKET_DELAY)
@@ -391,3 +452,150 @@ if __name__ == "__main__":
                 traceback.print_exc()
         else:
             print("Connection failed. Invalid Slack token or bot ID?")
+
+
+def init_db():
+    conn = None
+    try:
+        conn = mysql.connector.connect(host=DB_HOST,
+                                       database=DB_NAME,
+                                       user=DB_USER,
+                                       password=DB_PASSWORD)
+    except Error as e:
+        print("Failed to connect to db: %s" % e)
+    return conn
+
+
+def select(query, conn):
+    cursor = conn.cursor()
+    cursor.execute(query)
+    records = cursor.fetchall()
+    cursor.close()
+    return records
+
+
+def get_id(table, slack_id_column, slack_id, conn):
+    cursor = conn.cursor()
+    query = "select id from %s where %s = '%s'" % (table, slack_id_column, slack_id)
+    cursor.execute(query)
+    records = cursor.fetchall()
+    cursor.close()
+    if records:
+        return records
+    return False
+
+
+def insert(table, columns, values, slack_id_column, slack_id, conn):
+    item_id = get_id(table, slack_id_column, slack_id, conn)
+    if item_id:
+        return item_id[0][0]
+    cursor = conn.cursor()
+    if isinstance(columns, list):
+        columns = ", ".join(columns)
+    if isinstance(values, list):
+        for index, value in enumerate(values):
+            if isinstance(value, int):
+                print("inserting %s at index %s" % (unicode(value), index))
+                values[index] = unicode(value)
+            else:
+                values[index] = value.replace(u"'", u"\'")
+        print(values)
+        values = u", ".join("'" + value + "'" for value in values)
+    query = "insert into %s(%s) values (%s)" % (table, columns, values)
+    print(query)
+    cursor.execute(query)
+    conn.commit()
+    item_id = get_id(table, slack_id_column, slack_id, conn)
+    cursor.close()
+    return item_id[0][0]
+
+
+def get_message_id(timestamp, user_id, conn):
+    cursor = conn.cursor()
+    query = "select id from tbl_messages where timestamp = %s and user_id = %s"
+    cursor.execute(query, (timestamp, user_id))
+    records = cursor.fetchall()
+    cursor.close()
+    if records:
+        return records[0][0]
+    return False
+
+
+def insert_message(team_id, channel_id, user_id, timestamp, text, conn):
+    message_id = get_message_id(timestamp, user_id, conn)
+    if message_id:
+        return message_id
+    cursor = conn.cursor()
+    query = "insert into tbl_messages (team_id, channel_id, user_id, timestamp, text) values (%s, %s, %s, %s, %s)"
+    cursor.execute(query, (team_id, channel_id, user_id, timestamp, text))
+    conn.commit()
+    cursor.close()
+    message_id = get_message_id(timestamp, user_id, conn)
+    return message_id
+
+
+def insert_file(file, conn):
+    cursor = conn.cursor()
+    query = "insert into tbl_files (message_id, mimetype, thumb_800, thumb_80, permalink, permalink_public, name) values (%s, %s, %s, %s, %s, %s, %s)"
+    values = (
+        file['message_id'],
+        file['mimetype'],
+        file.get('thumb_800'),
+        file.get('thumb_80'),
+        file.get('permalink'),
+        file.get('permalink_public'),
+        file.get('name'),
+    )
+    print(query % values)
+    cursor.execute(query, values)
+    conn.commit()
+    cursor.close()
+
+
+def handle_files(files, message_id, conn):
+    for file_data in files:
+        file = {
+            'message_id': message_id,
+            'mimetype': file_data['mimetype'],
+        }
+        if 'thumb_800' in file_data.keys():
+            file['thumb_800'] = file_data['thumb_800']
+        if 'thumb_80' in file_data.keys():
+            file['thumb_80'] = file_data['thumb_80']
+        if 'permalink' in file_data.keys():
+            file['permalink'] = file_data['permalink']
+        if 'permalink_public' in file_data.keys() and 'is_public' in file_data.keys() and file_data['is_public'] == True:
+            file['permalink_public'] = file_data['permalink_public']
+        if 'name' in file_data.keys():
+            file['name'] = file_data['name']
+        elif 'title' in file_data.keys():
+            file['name'] = file_data['title']
+        insert_file(file, conn)
+
+
+def handle_user_profile(user_profile, user_id, conn):
+    subs = []
+    query = "update tbl_users set"
+    if "name" in user_profile.keys():
+        query += " username = %s,"
+        subs.append(user_profile['name'])
+    if "real_name" in user_profile.keys():
+        query += " full_name = %s,"
+        subs.append(user_profile['real_name'])
+    if "image_72" in user_profile.keys():
+        query += " avatar_url = %s,"
+        subs.append(user_profile['image_72'])
+    if not subs:
+        return
+    cursor = conn.cursor()
+    query = query[:-1]
+    query += " where id = %s"
+    subs.append(user_id)
+    print(query % tuple(subs))
+    cursor.execute(query, tuple(subs))
+    conn.commit()
+    cursor.close()
+
+
+if __name__ == "__main__":
+    main()
--- moonbeamlog2db.py
+++ moonbeamlog2db.py
@@ -85,6 +85,69 @@ def insert_message(team_id, channel_id, user_id, timestamp, text, conn):
     return message_id
 
 
+def insert_file(file, conn):
+    cursor = conn.cursor()
+    query = "insert into tbl_files (message_id, mimetype, thumb_800, thumb_80, permalink, permalink_public, name) values (%s, %s, %s, %s, %s, %s, %s)"
+    values = (
+        file['message_id'],
+        file['mimetype'],
+        file.get('thumb_800'),
+        file.get('thumb_80'),
+        file.get('permalink'),
+        file.get('permalink_public'),
+        file.get('name'),
+    )
+    print(query % values)
+    cursor.execute(query, values)
+    conn.commit()
+    cursor.close()
+
+
+def handle_files(files, message_id, conn):
+    for file_data in files:
+        file = {
+            'message_id': message_id,
+            'mimetype': file_data['mimetype'],
+        }
+        if 'thumb_800' in file_data.keys():
+            file['thumb_800'] = file_data['thumb_800']
+        if 'thumb_80' in file_data.keys():
+            file['thumb_80'] = file_data['thumb_80']
+        if 'permalink' in file_data.keys():
+            file['permalink'] = file_data['permalink']
+        if 'permalink_public' in file_data.keys() and 'is_public' in file_data.keys() and file_data['is_public'] == True:
+            file['permalink_public'] = file_data['permalink_public']
+        if 'name' in file_data.keys():
+            file['name'] = file_data['name']
+        elif 'title' in file_data.keys():
+            file['name'] = file_data['title']
+        insert_file(file, conn)
+
+
+def handle_user_profile(user_profile, user_id, conn):
+    subs = []
+    query = "update tbl_users set"
+    if "name" in user_profile.keys():
+        query += " username = %s,"
+        subs.append(user_profile['name'])
+    if "real_name" in user_profile.keys():
+        query += " full_name = %s,"
+        subs.append(user_profile['real_name'])
+    if "image_72" in user_profile.keys():
+        query += " avatar_url = %s,"
+        subs.append(user_profile['image_72'])
+    if not subs:
+        return
+    cursor = conn.cursor()
+    query = query[:-1]
+    query += " where id = %s"
+    subs.append(user_id)
+    print(query % tuple(subs))
+    cursor.execute(query, tuple(subs))
+    conn.commit()
+    cursor.close()
+
+
 def handle_json(json_buffer, conn, cache=None):
     if not cache:
         cache = {
@@ -113,6 +176,13 @@ def handle_json(json_buffer, conn, cache=None):
         return cache
     slack_channel_id = data["channel"]
     text = data["text"]
+    files = []
+    if "files" in data.keys():
+        files = data['files']
+    if "file" in data.keys():
+        files.append(data['file'])
+    if files:
+        print("Found file(s): %s" % files)
 
     if not slack_team_id in cache["teams"].keys():
         team_id = insert("tbl_teams", "slack_team_id", slack_team_id, "slack_team_id", slack_team_id, conn)
@@ -130,7 +200,12 @@ def handle_json(json_buffer, conn, cache=None):
     else:
         channel_id = cache["channels"][slack_channel_id]
     message_id = insert_message(team_id, channel_id, user_id, timestamp, text, conn)
-    print("channel: %s, user: %s, team: %s, message: %s" % (channel_id, user_id, team_id, message_id))
+    handle_files(files, message_id, conn)
+    if "user_profile" in data.keys():
+        handle_user_profile(data['user_profile'], user_id, conn)
+    #print("channel: %s, user: %s, team: %s, message: %s" % (channel_id, user_id, team_id, message_id))
+    if message_id % 100 == 0:
+        print("channel: %s, user: %s, team: %s, message: %s" % (channel_id, user_id, team_id, message_id))
     return cache
 
 
