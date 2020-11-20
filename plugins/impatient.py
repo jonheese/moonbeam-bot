04650704555c186889d5e14576d7e70158e4e9b3
Jon Heese <git@jonheese.com>
2020-11-20 11:44:11 -0500

    Add channel to key for taunt tracking, update default taunt thresholds

--- plugins/impatient.py
+++ plugins/impatient.py
@@ -5,8 +5,8 @@ from time import time
 class ImpatientPlugin(plugin.NoBotPlugin):
     def __init__(self, web_client, plugin_config):
         super().__init__(web_client=web_client, plugin_config=plugin_config)
-        self.__typing_by_user = {}
-        self.__taunts_by_user = {}
+        self.__typing_by_user_channel = {}
+        self.__taunts_by_user_channel = {}
 
 
     def __get_quip(self, user):
@@ -16,35 +16,37 @@ class ImpatientPlugin(plugin.NoBotPlugin):
 
 
     def receive(self, message):
-        if 'user' in message.keys():
-            self.__typing_by_user[message['user']] = time()
+        if 'user' in message.keys() and 'channel' in message.keys():
+            self.__typing_by_user_channel[f"{message['user']}_{message['channel']}"] = time()
         return []
 
 
     def typing(self, data):
         responses = []
         user = data['user']
+        channel = data['channel']
+        user_channel = f"{user}_{channel}"
         min = self._config['RESPONSE_THRESHOLD_SECS']
         max = self._config['MAX_THRESHOLD_SECS']
         cooldown = self._config['COOLDOWN_SECS']
         now = time()
-        last_taunt = self.__taunts_by_user.get(user, 0)
+        last_taunt = self.__taunts_by_user_channel.get(user_channel, 0)
         if now - last_taunt > cooldown:
-            last_typing_start = self.__typing_by_user.get(user, 0)
+            last_typing_start = self.__typing_by_user_channel.get(user_channel, 0)
             interval = now - last_typing_start
             if interval > max:
-                self.__typing_by_user[user] = now
-                self._log.debug(f"Setting typing_by_user for user {user} to now because {interval} is greater than {max}")
+                self.__typing_by_user_channel[user_channel] = now
+                self._log.debug(f"Setting typing start timestamp for user {user} in {channel} to now because {interval} is greater than {max}")
             elif interval > min:
-                self._log.debug(f"Taunting user {user} because {interval} is between {min} and {max}")
+                self._log.debug(f"Taunting user {user} in channel {channel} because {interval} is between {min} and {max}")
                 responses.append(
                     {
-                        'channel': data['channel'],
+                        'channel': channel,
                         'text': self.__get_quip(user),
                     }
                 )
-                self.__typing_by_user[user] = 0
-                self.__taunts_by_user[user] = now
+                self.__typing_by_user_channel[user_channel] = 0
+                self.__taunts_by_user_channel[user_channel] = now
             else:
                 self._log.debug(f"Not taunting user {user} because {interval} is not between {min} and {max}")
         else:
