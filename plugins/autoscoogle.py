13c6fe5eed603e3ccffd9c2708c8dff07ec77a7b
Jon Heese <git@jonheese.com>
2020-12-16 13:56:32 -0500

    Make autoscoogle work a little better for "who" triggers, add gracious plugin

--- plugins/autoscoogle.py
+++ plugins/autoscoogle.py
@@ -10,28 +10,29 @@ class AutoScooglePlugin(plugin.NoBotPlugin):
             return False
         responses = []
         user = request['user']
-        if user:
-        # if user in self._config.get("AUTOSCOOGLE_USERS"):
-            # Potential auto-scoogle request
-            subject = None
-            text = request.get('text', '').lower()
-            for trigger in self._config['AUTOSCOOGLE_TRIGGERS']:
-                if trigger in text.lower():
-                    subject = trigger.join(text.split(trigger)[1:])
-                    if "?" in subject:
-                        subject = subject.split("?")[0]
-                    break
-            if subject:
-                res = requests.get(f'https://www.google.com/search?q={quote(subject)}&btnI')
-                res.raise_for_status()
-                if res.url.startswith('https://www.google.com/url?q='):
-                    url = "=".join(res.url.split('=')[1:])
+        subject = None
+        text = request.get('text', '').lower()
+        for trigger in self._config['AUTOSCOOGLE_TRIGGERS']:
+            if trigger in text.lower():
+                if trigger.startswith("who"):
+                    subject = " ".join(text.split(trigger)[1:])
                 else:
-                    url = res.url
-                responses.append(
-                    {
-                        'channel': request['channel'],
-                        'text': f"I Auto-Scoogled that for you:\n{url}",
-                    }
-                )
+                    subject = trigger.join(text.split(trigger)[1:])
+                if "?" in subject:
+                    subject = subject.split("?")[0]
+                self._log.debug(f"################## Subject is {subject}")
+                break
+        if subject:
+            res = requests.get(f'https://www.google.com/search?q={quote(subject)}&btnI')
+            res.raise_for_status()
+            if res.url.startswith('https://www.google.com/url?q='):
+                url = "=".join(res.url.split('=')[1:])
+            else:
+                url = res.url
+            responses.append(
+                {
+                    'channel': request['channel'],
+                    'text': f"I Auto-Scoogled that for you:\n{url}",
+                }
+            )
         return responses
---
+++ plugins/gracious.py
@@ -0,0 +1,45 @@
+from . import plugin
+from profanity import profanity
+import json
+import os
+
+
+class GraciousPlugin(plugin.NoBotPlugin):
+    def __init__(self, web_client, plugin_config):
+        super().__init__(web_client=web_client, plugin_config=plugin_config)
+        pleasantries_file = self._config.get('PLEASANTRIES_FILE')
+        if pleasantries_file:
+            with open(os.path.join(os.path.dirname(__file__), "..", pleasantries_file), 'r') as f:
+                self.__pleasantries = json.load(f)
+        else:
+            raise RuntimeError(f"Unable to locate pleasantries file {pleasantries_file}")
+
+
+    def receive(self, request):
+        if super().receive(request) is False:
+            return False
+        responses = []
+        request_text = request['text']
+        rude = False
+        if "moonbeam" in request_text.lower():
+            for word in request_text.split():
+                if profanity.contains_profanity(word):
+                    responses.append(
+                        {
+                            'channel': request['channel'],
+                            'text': f"There's no need to be rude, <@{request['user']}>! :face_with_raised_eyebrow:",
+                        }
+                    )
+                    rude = True
+                    break
+            if not rude:
+                for pleasantry in self.__pleasantries:
+                    if pleasantry in request_text.lower():
+                        responses.append(
+                            {
+                                'channel': request['channel'],
+                                'text': f"You're welcome, <@{request['user']}>! :grin:",
+                            }
+                        )
+                        break
+        return responses
