d66f606598652b39964c6b0be080afc661c808f9
Jon Heese <git@jonheese.com>
2021-04-14 11:06:57 -0400

    Fix a bunch of weather plugin bugs and features

--- plugins/weather.py
+++ plugins/weather.py
@@ -40,7 +40,12 @@ class WeatherPlugin(plugin.NoBotPlugin):
         if not api_key:
             self._log.debug(f"Didn't find an API key for source {self.source}")
             return []
-        response = requests.get(url).json()
+        try:
+            response = requests.get(url).json()
+        except simplejson.errors.JSONDecodeError:
+            self._log.error("Got invalid JSON from API:")
+            self._log.error(requests.get(url))
+            return []
         if self.source == 'weatherbit':
             if 'data' not in response:
                 self._log.debug(
@@ -55,26 +60,24 @@ class WeatherPlugin(plugin.NoBotPlugin):
             }
         else:
             if response.get("status") != "success":
-                return self.get_forecast(days, datecode, '92328')
+                return self.get_weather_data(days, datecode, '92328')
             return response.get('weatherInfo').get(f'ZU{zipcode}')
 
-
-    def get_forecast(self, days, datecode, zipcode):
-        forecast = []
-        weather_info = self.get_weather_data(days, datecode, zipcode)
+    def get_forecast_table(self, forecast, weather_info):
         if not weather_info:
             return forecast
-        forecast.append(f"Forecast for {weather_info.get('name')}:")
-        forecast.append('```')
-        forecast.append('Day     High     Low     Rain   Snow')
-        forecast.append('=====================================')
+        block = []
+        block.append('```')
+        block.append('Date       High    Low    Rain  Snow')
+        block.append('=====================================')
         for day in weather_info.get('wxInfo'):
-            dow = datetime.strptime(
+            date = datetime.strptime(
                 day.get(
                     self.fields.get(self.source).get('date')
                 )[:10],
                 '%Y-%m-%d'
-            ).strftime('%a')
+            )
+            dow = f"{date.strftime('%m/%d')} {date.strftime('%a')[0]}"
             max_temp = "{:-03.1f}".format(
                 float(day.get(
                     self.fields.get(self.source).get('max_temp')
@@ -100,13 +103,85 @@ class WeatherPlugin(plugin.NoBotPlugin):
             prcp_padding = (4 - len(prcp)) * " "
             snow_padding = (4 - len(snow)) * " "
             if snow:
-                forecast.append(f"{dow}:  {high_padding}{max_temp}°F  "
-                    f"{low_padding}{min_temp}°F {prcp_padding}  {prcp}" +
-                    f"{snow_padding}  {snow}")
+                block.append(f"{dow}: {high_padding}{max_temp}°F "
+                    f"{low_padding}{min_temp}°F  {prcp_padding}{prcp} " +
+                    f"{snow_padding}{snow}")
             else:
-                forecast.append(f"{dow}:  {high_padding}{max_temp}°F  "
-                    f"{low_padding} {min_temp}°F {prcp_padding}  {prcp}")
-        forecast.append('```')
+                block.append(f"{dow}: {high_padding}{max_temp}°F "
+                    f"{low_padding}{min_temp}°F {prcp_padding}{prcp}")
+        block.append('```')
+        forecast.append(
+            {
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': "\n".join(block),
+                }
+            }
+        )
+        if len(forecast) > 0:
+            return forecast
+        else:
+            return []
+
+
+    def get_forecast(self, days, datecode, zipcode, table=False):
+        forecast = []
+        weather_info = self.get_weather_data(days, datecode, zipcode)
+        if not weather_info:
+            return forecast
+        forecast.append(
+            {
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': f"Forecast for {weather_info.get('name')}:",
+                }
+            }
+        )
+        if table:
+            return self.get_forecast_table(forecast, weather_info)
+        for day in weather_info.get('wxInfo'):
+            dow = datetime.strptime(
+                day.get(
+                    self.fields.get(self.source).get('date')
+                )[:10],
+                '%Y-%m-%d'
+            ).strftime('%A %B %d, %Y')
+            max_temp = "{:-03.1f}".format(
+                float(day.get(
+                    self.fields.get(self.source).get('max_temp')
+                ))
+            )
+            min_temp = "{:-03.1f}".format(
+                float(day.get(
+                    self.fields.get(self.source).get('min_temp')
+                ))
+            )
+            prcp = day.get(self.fields.get(self.source).get('prcp'))
+            snow = day.get(self.fields.get(self.source).get('snow'))
+            if not prcp or float(prcp) == 0.0:
+                prcp = None
+            else:
+                prcp = '{:02.2f}"'.format(float(prcp))
+            if not snow or float(snow) == 0.0 or float(snow) == -999.99:
+                snow = None
+            else:
+                snow = '{:02.2f}"'.format(float(snow))
+            text = f"*{dow}:*\n_Temperature:_ {min_temp}°F - {max_temp}°F"
+            if prcp:
+                text += f"\n_Rain:_ {prcp}"
+            if snow:
+                text += f"\n_Snow:_ {snow}"
+            forecast.append(
+                {
+                    'type': 'section',
+                    'text': {
+                        'type': 'mrkdwn',
+                        'text': text,
+                    },
+                }
+            )
         if days > 0:
             return forecast
         else:
@@ -147,6 +222,12 @@ class WeatherPlugin(plugin.NoBotPlugin):
         return self._config.get('SOURCE', 'wt360')
 
 
+    def is_table_request(self, command):
+        for word in command:
+            if word.lower() == 'table':
+                return True
+        return False
+
     def get_days(self, command):
         numbered = {
             "hours": 1/24,
@@ -183,7 +264,7 @@ class WeatherPlugin(plugin.NoBotPlugin):
         responses = []
         if request['text'].lower().startswith("moonbeam weather"):
             self._log.debug(f"Got weather request: {request['text']}")
-            command = request['text'].split()
+            command = request['text'].replace('<tel:', '').replace('>', '').split()
             days = self.get_days(command)
             self.source = self.get_source(command)
             if days > 30:
@@ -201,7 +282,7 @@ class WeatherPlugin(plugin.NoBotPlugin):
                 else:
                     datecode = datetime.now().strftime('%Y%m%d000000')
                 try:
-                    forecast = self.get_forecast(days, datecode, zipcode)
+                    forecast = self.get_forecast(days, datecode, zipcode, table=self.is_table_request(command))
                 except Exception as e:
                     self._log.error(traceback.format_exc())
                     forecast = [
@@ -209,12 +290,19 @@ class WeatherPlugin(plugin.NoBotPlugin):
                         f"forecast you requested, <@{request['user']}>"
                     ]
             if len(forecast) > 0:
-                responses.append(
-                    {
+                if isinstance(forecast[0], str):
+                    responses.append(
+                        {
+                            'channel': request['channel'],
+                            'text': "\n".join(forecast),
+                        }
+                    )
+                else:
+                    response = {
                         'channel': request['channel'],
-                        'text': "\n".join(forecast),
+                        'blocks': forecast,
                     }
-                )
+                    responses.append(response)
             else:
                 responses.append(
                     {
