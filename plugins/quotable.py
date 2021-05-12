3be8cb734d0eca8ebe87135525da4891282f9bd6
Jon Heese <git@jonheese.com>
2021-05-12 16:10:41 -0400

    Add quotable stats, fix some quotes

--- plugins/quotable.py
+++ plugins/quotable.py
@@ -70,6 +70,40 @@ class QuotablePlugin(plugin.NoBotPlugin):
         return responses
 
 
+    def __get_stats(self):
+        max_length = 0
+        scores = {}
+        quotes = self.__read_quotes()
+        for quote in quotes:
+            split_quote = quote.split(" - ")
+            split_attribution = split_quote[1].split(",")
+            person = split_attribution[0]
+            quote = split_quote[0]
+            attribution = ", ".join(split_quote[1:])
+            split_person = person.split()
+            person = split_person[0] + " " + split_person[len(split_person) - 1]
+            if person in scores.keys():
+                scores[person] = scores[person] + 1
+            else:
+                scores[person] = 1
+            if len(person) > max_length:
+                max_length = len(person)
+
+        sorted_scores = sorted(scores.items(), key=lambda item: item[1])
+        sorted_scores.reverse()
+        scores = {k: v for k, v in sorted_scores}
+
+        text = '```'
+        for person, count in scores.items():
+            percent = (count / len(quotes)) * 100
+            spaces = max_length - len(person)
+            count_percent = f'{count} ({percent:.2f}%)'
+            count_spacing = 12 - len(count_percent)
+            text = text + f"{person}: {spaces*' '} {count_percent}{' ' * count_spacing} {'#' * count}\n"
+        text = text + '```'
+        return text
+
+
     def receive(self, request):
         if super().receive(request) is False:
             return False
@@ -78,10 +112,13 @@ class QuotablePlugin(plugin.NoBotPlugin):
         channel = request['channel']
         user = request['user']
         if "quotable" in request['text'].lower():
-            quote = self.__get_quote_message(channel=channel)
-            self._log.info("printing quotable:")
-            self._log.info(quote['text'])
-            responses.append(quote)
+            if "stats" in request['text'].lower():
+                responses.append(self.__build_message(self.__get_stats(), request['channel']))
+            else:
+                quote = self.__get_quote_message(channel=channel)
+                self._log.info("printing quotable:")
+                self._log.info(quote['text'])
+                responses.append(quote)
         if text.lower().startswith('moonbeam') and "add-quote" in request['text'].lower():
             command_index = -1
             index = -1
@@ -124,3 +161,4 @@ class QuotablePlugin(plugin.NoBotPlugin):
             else:
                 responses.append(self.__build_message(f":thinking_face: Ummm... I don't think there's any quote to {text.lower().split()[0]}...", channel))
         return responses
+
